6784d1f215a37eba8328970ec1b537d6cdbbf72b
Boglarka Barabas <taikatta@gmail.com>
2019-12-30 14:50:27 +0000

    Refactoring main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,89 +1,78 @@
+const api_key = '77d3ZAFkI96vA4EA6dPuFyNBI0sEDuI87tbTjTY9';
+
 // Init date input
 // Disable future dates, the solution is from: https://stackoverflow.com/
-dateinput.max = new Date().toISOString().split("T")[0];
+
 document.getElementById('dateinput').valueAsDate = new Date();
 
 document.getElementById('button').addEventListener('click', updatePage);
 
-
 function updatePage() {
-    document.getElementById('data-pro').textContent = "";
-    document.getElementById('usa-date').textContent = "";
-    document.getElementById('there_is_no_picture').textContent = "";
     const date = document.getElementById('dateinput').value;
-    
-    var usaTime = formatDate(new Date().toLocaleString("en-US", {timeZone: "America/New_York"}).split("T")[0]);
+    const valid = dateValidator(date);
 
-
-    // Warning if date is earlier then 1995-06-16 or is a future date
-    if(date < "1995-06-16" || date > new Date().toISOString().split("T")[0]) {
-        document.getElementById('there_is_no_picture').textContent = "Please enter a date between 16/06/1995 and today!";
+    if (valid) {
+        document.getElementById('validDate').innerHTML = valid;
         document.getElementById('title').textContent = "";
         document.getElementById('explanation').textContent = "";
         document.getElementById('yourneo').textContent = "";
         return;
     }
-
-    // Warning if the user is in a different timezone than NASA and NASA hasn't updated the photo yet
-    if(usaTime < date) {
-        document.getElementById('usa-date').innerHTML = `NASA date is still ${usaTime}, please select another date.`;
-       return;
-    }
+    document.getElementById('validDate').innerHTML = "";
 
     // Build url
     const nasaUrl = new URL('https://api.nasa.gov/planetary/apod');
-    nasaUrl.searchParams.set('api_key', '77d3ZAFkI96vA4EA6dPuFyNBI0sEDuI87tbTjTY9');
+    nasaUrl.searchParams.set('api_key', api_key);
     nasaUrl.searchParams.set('date', date);
 
     // Call api
     fetch(nasaUrl.toString())
-        //.then(handleErrors)
         .then(response => response.json())
         .then(response => {
             document.getElementById("expl_text").className = "expl_text1";
 
-            const APOD = document.getElementById('photo')
-            const media = document.getElementById('media_from_nasa')
+            const APOD = document.getElementById('photo');
+            const media = document.getElementById('media_from_nasa');
 
             //Check if today's media is image or video
             const mediaElement = response.media_type === 'image' ? 'img' : 'iframe';
 
             element = document.createElement(mediaElement);
             element.setAttribute('src', response.url);
-            element.setAttribute('id', 'media_from_nasa')
+            element.setAttribute('id', 'media_from_nasa');
             if (media) {
-                APOD.replaceChild(element, media)
+                APOD.replaceChild(element, media);
             } else {
-                APOD.appendChild(element)
+                APOD.appendChild(element);
             }
             document.getElementById('title').textContent = response.title;
             document.getElementById('explanation').textContent = response.explanation;
         });
 
     // Build url
-    let START_DATE = date;
-    let END_DATE = date;
-    const neoUrl = new URL('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + 
-        START_DATE + '&end_date=' + 
-        END_DATE + '&api_key=77d3ZAFkI96vA4EA6dPuFyNBI0sEDuI87tbTjTY9');
- 
+    const neoUrl = new URL('https://api.nasa.gov/neo/rest/v1/feed');
+    neoUrl.searchParams.set('start_date', date);
+    neoUrl.searchParams.set('end_date', date);
+    neoUrl.searchParams.set('api_key', api_key);
+
+
     // Call api
     fetch(neoUrl.toString())
         .then(response => response.json())
         .then(response => {
-            let data = response.near_earth_objects[START_DATE];
+            let data = response.near_earth_objects[date];
             let count = response.element_count;
             var i = 0;
             var hazardous = false;
             var biggest = 0;
             do {
                 // Check for potentially hazardous asteroid
-                if(data[i].is_potentially_hazardous_asteroid === true) {
+                if (data[i].is_potentially_hazardous_asteroid === true) {
                     hazardous = true;
                     var hazardous_name = data[i].name;
                 }
                 // Find the biggest asteroid
-                if(biggest < data[i].estimated_diameter.meters.estimated_diameter_max) {
+                if (biggest < data[i].estimated_diameter.meters.estimated_diameter_max) {
                     var biggest = data[i].estimated_diameter.meters.estimated_diameter_max;
                 }
                 i++;
@@ -93,12 +82,11 @@ function updatePage() {
             var danger_name = hazardous ? ", its name is: " + hazardous_name + "." : ".";
 
             document.getElementById('yourneo').innerHTML = "<h4>Near Earth Objects on " + date + ":</h4>" +
-                "The number of near Earth objects is: " + response.element_count +"<br>" +
+                "The number of near Earth objects is: " + response.element_count + "<br>" +
                 "Estimated maximum diameter is: " + biggest.toFixed(2) + " meter.<br>" +
                 "There " + danger + " potentially hazardous asteroid among them" + danger_name;
         });
-};
-
+}
 
 function formatDate(date) {
     var d = new Date(date),
@@ -106,10 +94,20 @@ function formatDate(date) {
         day = '' + d.getDate(),
         year = d.getFullYear();
 
-    if (month.length < 2) 
+    if (month.length < 2)
         month = '0' + month;
-    if (day.length < 2) 
+    if (day.length < 2)
         day = '0' + day;
 
     return [year, month, day].join('-');
+}
+
+function dateValidator(date) {
+    if (date < "1995-06-16" || date > new Date().toISOString().split("T")[0]) {
+        return "Please enter a date between 16/06/1995 and today!";
+    }
+    var usaTime = formatDate(new Date().toLocaleString("en-US", { timeZone: "America/New_York" }).split("T")[0]);
+    if (usaTime < date) {
+        return "NASA date is still " + usaTime + ", please select another date.";
+    }
 }
